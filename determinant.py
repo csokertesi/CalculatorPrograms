625c434f6967cf79f41c0c52a608067e5066e497
Csongor Kertesi <48154331+csokertesi@users.noreply.github.com>
2025-11-17 17:43:09 +0100

    Refactor

--- determinant.py
+++ determinant.py
@@ -1,32 +1,35 @@
-def input_matrix():
-    size = int(input("Size: "))
-    matrix = [[int(input(f"{x}, {y}: ")) for x in range(size)] for y in range(size)]
+def input_matrix(size):
+    matrix = [[int(input(f"{x},{y}: ")) for x in range(size)] for y in range(size)]
     return matrix
-
-def print_matrix(matrix):
-    size = len(matrix)
-    print("-" * 2 * size)
-    for row in range(size):
-        print(''.join([f"{x} " for x in matrix[row]]))
-    print("-" * 2 * size)
-    
 
 def det(matrix):
     if len(matrix) == 0 or len(matrix[0]) != len(matrix):
         raise ValueError("Non-square matrix!")
 
     size = len(matrix)
-    
+
+    # 2x2 matrix determinant
     if size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
     determinant = 0
+
+    # Loop first row
+    # i: index (column)
+    # coeff: value
     for i, coeff in enumerate(matrix[0]):
+        # Negate every second coefficient
         coeff *= 1 if i % 2 == 0 else -1
-        subMatrix = [
+
+        # Construct sub-matrix without first row and current column
+        sub_matrix = [
                 [x for x in matrix[r][0:i]+matrix[r][i+1:size]] for r in range(1, size)
             ]
-        determinant += coeff * det(subMatrix)
-    return determinant
         
-print(det(input_matrix()))
+        # Add product of coefficient and the determinant of the sub-matrix to the sum
+        determinant += coeff * det(sub_matrix)
+    
+    return determinant
+
+
+print(det(input_matrix(input("Size: ")))
